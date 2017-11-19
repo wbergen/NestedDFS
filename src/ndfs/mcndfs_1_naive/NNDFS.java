8485d8615c2facba049330ebdcdd8493e631fc3c
emanuele vineti <emanuele.vineti@gmail.com>
2017-11-19 15:32:50 +0100

    lol

--- src/ndfs/mcndfs_1_naive/NNDFS.java
+++ src/ndfs/mcndfs_1_naive/NNDFS.java
@@ -15,14 +15,7 @@ public class NNDFS implements NDFS {
         create an array of workers
         and shared data structures
     */
-<<<<<<< HEAD
     private final Worker worker[];
-=======
-    // private final Worker worker;
-
-    private int nrWorkers;
-    private final Worker[] workers;
->>>>>>> 19b4de792952908b927c5acf5bbae1158a6db99a
 
     /**
      * Constructs an NDFS object using the specified Promela file.
@@ -37,18 +30,10 @@ public class NNDFS implements NDFS {
             XXXXXXXXXXXXXXXXXXXXXXXXXX
             We need to create locally nrWorkers
         */
-<<<<<<< HEAD
-            this.worker = new Worker[nrWorkers];
+        this.worker = new Worker[nrWorkers];
+        
         for( int i = 0; i < nrWorkers ; i++){
             this.worker[i] = new Worker(promelaFile);     
-=======
-           
-        this.workers = new Worker[nrWorkers];
-        this.nrWorkers = nrWorkers;
-
-        for (int i = 0; i < nrWorkers; i++) {
-            this.workers[i] = new Worker(promelaFile);
->>>>>>> 19b4de792952908b927c5acf5bbae1158a6db99a
         }
     }
 
@@ -58,28 +43,16 @@ public class NNDFS implements NDFS {
             XXXXXXXXXXXXXXXXXXXXXXXXXX
             We need to run nrWorkers
         */
-<<<<<<< HEAD
         for( int i = 0; i < this.worker.length; i++){
             this.worker[i].run();
         }
-        return worker.getResult();
-=======
-        for (int i = 0; i < nrWorkers; i++) {
-            workers[i].run();
-        }
-
-        // Need some way to collect the results..
-        boolean res = false;
-        for (int i = 0; i < nrWorkers; i++) {
-            if (workers[i].getResult()) {
-                res = true;
-            }
-        }
-        return res;
         
+        // Sync barrier
+        for( int i = 0; i < this.worker.length; i++){
+            this.worker[i].join();
+        }
+        //return result
+        return this.worker[0].getResult();
 
-        // worker.run();
-        // return worker.getResult();
->>>>>>> 19b4de792952908b927c5acf5bbae1158a6db99a
     }
 }
